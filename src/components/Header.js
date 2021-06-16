1c3a1e018ba948e8c0c686a4c909315a798b14d4
Alex Katopodis <alexkatopodis@live.com>
2021-06-15 23:19:08 -0300

    incluid method for Login, and Sign Out, trasition page for home

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import { auth, provider } from "./firebase";
 import { 
     selectUserName, 
     setUserLoginDetails, 
-    selectUserPhoto 
+    selectUserPhoto, 
+    setSignOutState
 } from "../features/user/userSlice";
+import { useEffect } from "react";
 
 
 const Header = (props) => {
@@ -16,7 +18,17 @@ const Header = (props) => {
     const userName = useSelector(selectUserName);
     const userPhoto = useSelector(selectUserPhoto);
 
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                setUser(user)
+                history.push('/home')
+            }
+        });
+    }, [userName]);
+         
       const handleAuth = () => {
+          if (!userName) {
           auth
           .signInWithPopup(provider)
           .then((result) => {
@@ -24,7 +36,13 @@ const Header = (props) => {
           }).catch((error) => {
               alert(error.message);
           });
-      };
+      } else if (userName) {
+          auth.signOut().then(() => {
+              dispatch(setSignOutState())
+              history.push('/')
+          }).catch((err) => alert(err.message))
+      }
+    };
 
       const setUser = (user) => {
           dispatch(
@@ -78,7 +96,12 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>                
             </NavMenu>
-            <UserImg src={userPhoto} alt={userName} />
+            <SignOut>
+                <UserImg src={userPhoto} alt={userName} />
+                <DropDown>
+                    <span onClick={handleAuth}>Sair</span>
+                </DropDown>
+            </SignOut>
             </>
     )}        
         </Nav>
@@ -198,7 +221,44 @@ transition: all .2s ease 0s;
 
 const UserImg = styled.img`
 height: 100%;
+`;
 
+const DropDown = styled.div`
+position: absolute;
+top: 48px;
+right: 0px;
+background: rgb(19, 19, 19);
+border: 1px solid rgba(151, 151, 151 0.34);
+border-radius: 4px;
+box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+padding: 10px;
+font-size: 14px;
+letter-spacing: 3px;
+width: 100px;
+opacity: 0;
+`;
+
+const SignOut = styled.div`
+position: relative;
+height: 48px;
+width: 48px;
+display: flex;
+cursor: pointer;
+align-items: center;
+justify-content: center;
+
+${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+}
+
+&:hover{
+    ${DropDown} {
+      opacity  : 1 ;
+      transition-duration: 1s;
+    }
+}
 `;
 
 export default Header;
